d0faadd24dcc6dcd80d10b3aad36fda1b13dc39d
Jorgen <scubajorgen@gmail.com>
2021-04-14 17:41:51 +0200

    Improvement and check

--- src/main/java/net/studioblueplanent/top25ozigenerator/Generator.java
+++ src/main/java/net/studioblueplanent/top25ozigenerator/Generator.java
@@ -46,6 +46,13 @@ public class Generator
     private List<Calibration>   calibrations;
     private String              template;
     
+    /**
+     * Calculate lat and lon based on RD coordinates. See method
+     * https://media.thomasv.nl/2015/07/Transformatieformules.pdf
+     * @param x Easting
+     * @param y Northing
+     * @return WGS84 lat/lon coordinate
+     */
     private LatLon RdToLanLon(double x, double y)
     {
         double dX;
@@ -59,58 +66,32 @@ public class Generator
 	dX = (x - 155000) * 0.00001;
 	dY = (y - 463000) * 0.00001;
 
-/*        
 	SomN = (3235.65389 * dY) + 
-                (-32.58297 * dX ^ 2) + 
-                (-0.2475 * dY ^ 2) + 
-                (-0.84978 * dX ^ 2 * dY) + 
-                (-0.0655 * dY ^ 3) + 
-                (-0.01709 * dX ^ 2 * dY ^ 2) + 
-                (-0.00738 * dX) + 
-                (0.0053 * dX ^ 4) + 
-                (-0.00039 * dX ^ 2 * dY ^ 3) + 
-                (0.00033 * dX ^ 4 * dY) + 
-                (-0.00012 * dX * dY)
+               (-32.58297  * Math.pow(dX, 2)) + 
+               (-0.24750   * Math.pow(dY, 2)) + 
+               (-0.84978   * Math.pow(dX, 2) * dY) + 
+               (-0.0655    * Math.pow(dY, 3)) + 
+               (-0.01709   * Math.pow(dX, 2) * Math.pow(dY, 2)) + 
+               (-0.00738   * dX) + 
+               (0.00530    * Math.pow(dX, 4)) + 
+               (-0.00039   * Math.pow(dX, 2) * Math.pow(dY, 3)) + 
+               (0.00033    * Math.pow(dX, 4) * dY) + 
+               (-0.00012   * dX * dY);
 	SomE = (5260.52916 * dX) + 
                 (105.94684 * dX * dY) + 
-                (2.45656 * dX * dY ^ 2) + 
-                (-0.81885 * dX ^ 3) + 
-                (0.05594 * dX * dY ^ 3) + 
-                (-0.05607 * dX ^ 3 * dY) + 
-                (0.01199 * dY) + 
-                (-0.00256 * dX ^ 3 * dY ^ 2) + 
-                (0.00128 * dX * dY ^ 4) + 
-                (0.00022 * dY ^ 2) + 
-                (-0.00022 * dX ^ 2) + 
-                (0.00026 * dX ^ 5)
-*/        
-        
-	SomN = (3235.65389 * dY) + 
-               (-32.58297 * dX*dX) + 
-               (-0.2475 * dY*dY) + 
-               (-0.84978 * dX * dX * dY) + 
-               (-0.0655 * dY * dY *dY) + 
-               (-0.01709 * dX *dX * dY *dY) + 
-               (-0.00738 * dX) + 
-               (0.0053 * dX * dX * dX * dX) + 
-               (-0.00039 * Math.pow(dX, 2) * Math.pow(dY, 3)) + 
-               (0.00033 * Math.pow(dX, 4) * dY) + 
-               (-0.00012 * dX * dY);
-	SomE = (5260.52916 * dX) + 
-                (105.94684 * dX * dY) + 
-                (2.45656 * dX * Math.pow(dY, 2)) + 
-                (-0.81885 * Math.pow(dX, 3)) + 
-                (0.05594 * dX * Math.pow(dY, 3)) + 
-                (-0.05607 * Math.pow(dX, 3) * dY) + 
-                (0.01199 * dY) + 
-                (-0.00256 * Math.pow(dX, 3) * Math.pow(dY, 2)) + 
-                (0.00128 * dX * Math.pow(dY, 4)) + 
-                (0.00022 /** Math.pow(dY, 2)*/) + 
-                (-0.00022 * Math.pow(dX, 2)) + 
-                (0.00026 * Math.pow(dX, 5));
+                (2.45656   * dX * Math.pow(dY, 2)) + 
+                (-0.81885  * Math.pow(dX, 3)) + 
+                (0.05594   * dX * Math.pow(dY, 3)) + 
+                (-0.05607  * Math.pow(dX, 3) * dY) + 
+                (0.01199   * dY) + 
+                (-0.00256  * Math.pow(dX, 3) * Math.pow(dY, 2)) + 
+                (0.00128   * dX * Math.pow(dY, 4)) + 
+                (0.00022   * Math.pow(dY, 2)) + 
+                (-0.00022  * Math.pow(dX, 2)) + 
+                (0.00026   * Math.pow(dX, 5));
 
-	ll.lat = 52.1551744 + (SomN / 3600.0);
-	ll.lon = 5.38720621 + (SomE / 3600.0);
+	ll.lat = 52.15517440 + (SomN / 3600.0);
+	ll.lon = 5.38720621  + (SomE / 3600.0);
 
         return ll;
     }
